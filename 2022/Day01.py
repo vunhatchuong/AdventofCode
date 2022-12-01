0e0bd1e76a7ff7d1e75ecdc09ead36a72b86e07d
ronny <ronnyvu321@gmail.com>
2022-12-01 21:43:12 +0700

    2022: Finish Day01

--- 2022/Day01.py
+++ 2022/Day01.py
@@ -1,12 +1,38 @@
-file = open("./input/Day01.in","r", encoding="UTF-8").read().splitlines()
+# My solution
+file = open("./input/Day01.in", "r", encoding="UTF-8").read().splitlines()
 
-# number_of_person = [line if line == " " else line for line in file]
-data = ['1','3','','9','5']
-output = []
-for i,line in enumerate(file):
-    if file[i+1] == "":
-        output.append[
-    
-    output[i] += line
+number_of_person = 0
+output = [0]
+for line in file:
+    if line == "":
+        number_of_person += 1
+        output.append(0)
+        continue
 
-print(file)
+    output[number_of_person] += int(line)
+
+print("Number of person", number_of_person)
+print("Max Calories", max(output))
+
+# Part two
+
+print("Sum of top 3 values", sum(sorted(output)[-3:]))
+
+
+## Optimized
+file = open("./input/Day01.in", "r", encoding="UTF-8").read()
+
+
+# Part one
+print(max(
+    sum(int(line) for line in part.splitlines())
+    for part in file.split("\n\n")
+        )
+    )
+
+# Part Two
+print(sum(sorted(
+    sum(int(line) for line in part.splitlines())
+    for part in file.split("\n\n")
+        )
+      [-3:]))
