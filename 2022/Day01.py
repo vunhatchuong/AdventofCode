fcdaef827255051bb860dacedd92ab3adaef9b54
ronny <ronnyvu321@gmail.com>
2022-12-01 15:32:09 +0700

    WIP: Day1

---
+++ 2022/Day01.py
@@ -0,0 +1,12 @@
+file = open("./input/Day01.in","r", encoding="UTF-8").read().splitlines()
+
+# number_of_person = [line if line == " " else line for line in file]
+data = ['1','3','','9','5']
+output = []
+for i,line in enumerate(file):
+    if file[i+1] == "":
+        output.append[
+    
+    output[i] += line
+
+print(file)
