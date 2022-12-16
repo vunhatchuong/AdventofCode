ba5d67625b32fc7e6f17c9c02f34a9cfa48583c5
ronny <ronnyvu321@gmail.com>
2022-12-16 17:09:44 +0700

    2022, day 02, part 1

--- 2022/Day02.py
+++ 2022/Day02.py
@@ -1,16 +1,24 @@
-import enum
+with open("./input/Day02.in","r",encoding="UTF-8") as f:
+    s = f.read()
 
-file = open("./input/Day02.in","r",encoding="UTF-8").read().splitlines()
+rounds = [x for x in s.splitlines()]
 
-class RPC(str,enum.Enum):
-    X = 1
-    Y = 2
-    Z = 3
+score = 0
 
-    def __init__(self,file) -> None:
-        self.file: list[str] = file
+OUTCOMES = {
+        "A X": 1+3,
+        "A Y": 2+6,
+        "A Z": 3+0,
+        "B X": 1+0,
+        "B Y": 2+3,
+        "B Z": 3+6,
+        "C X": 1+6,
+        "C Y": 2+0,
+        "C Z": 3+3
+        }
 
-    def find_winner(self):
-        pass
+for round in rounds:
+    print(round, OUTCOMES[round])
+    score += OUTCOMES[round]
 
-print(file)
+print(score)
